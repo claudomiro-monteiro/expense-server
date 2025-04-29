fe80720a0e87071fa2fb1f4a68ec94de441bf35b
claudomiro <claudomiromonteiro@gmail.com>
2025-04-29 12:08:29 -0300

    :tada: add host in server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,13 +1,13 @@
+// import { getSearchExpenseRoute } from './routes/get-search-expenses'
+import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
 import {
+  type ZodTypeProvider,
   serializerCompiler,
   validatorCompiler,
-  type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { createExpenseRoute } from './routes/create-expense'
 import { getExpenseRoute } from './routes/get-expenses'
-// import { getSearchExpenseRoute } from './routes/get-search-expenses'
-import fastifyCors from '@fastify/cors'
 import { getSearchDateExpenseRoute } from './routes/get-search-date-expenses'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -27,6 +27,7 @@ app.register(getSearchDateExpenseRoute)
 app
   .listen({
     port: 3333,
+    host: 'RENDER' in process.env ? `0.0.0.0` : `localhost`,
   })
   .then(() => {
     console.log('HTTP server running!')
